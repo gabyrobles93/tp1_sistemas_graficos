e047a4ed5ad9f37b997cce7bfbd9b4bab86bdad6
gabriel.robles <grobles@fi.uba.ar>
2020-06-13 00:01:07 -0300

    Soga de catapulta implementada.

--- js/world_objects/Catapult.js
+++ js/world_objects/Catapult.js
@@ -17,12 +17,66 @@ class Catapult {
         this.catapult_column_4 = new CatapultColumn();
 
         this.catapult_hub_principal = new Cilinder(0.4, this.CATAPULT_WHEEL_SEPARATION_2 * 0.84, true, MaterialsList.DEFAULT);
-        this.catapult_hub_rope = new Cilinder(0.25, this.CATAPULT_WHEEL_SEPARATION_2 * 0.84 * 0.5, true, MaterialsList.DEFAULT);
+        this.catapult_hub_rope = new Cilinder(0.25, this.CATAPULT_WHEEL_SEPARATION_2 * 0.84 * 0.6, true, MaterialsList.DEFAULT);
 
         this.catapult_arm = new CatapultArm();
+
+        this.catapult_crank_1 = new Cilinder(0.05, 1.2, true, MaterialsList.DEFAULT);
+        this.catapult_crank_2 = new Cilinder(0.05, 1.2, true, MaterialsList.DEFAULT);
     }
 
     draw(modelMatrix) {
+        this._drawStaticsParts(modelMatrix);
+        this._drawDynamicParts(modelMatrix);
+    }
+
+    setViewProjectionMatrix(projMatrix, viewMatrix) {
+        this.catapult_wheel_br.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_wheel_fr.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_wheel_bl.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_wheel_fl.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_floor.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_column_1.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_column_2.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_column_3.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_column_4.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_hub_principal.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_hub_rope.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_arm.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.catapult_crank_1.setViewProjectionMatrix(projMatrix, viewMatrix);
+    }
+
+    // Private
+
+    _drawDynamicParts(modelMatrix) {
+        // REFERENCIA NO DIBUJABLE: CENTRO DEL EJE MOVIL, ENTRE LAS MANIVELAS
+        var rope_lookat = mat4.clone(modelMatrix);
+        mat4.translate(rope_lookat, rope_lookat, [this.CATAPULT_WHEEL_SEPARATION_1 * 0.35, 1.6, 0.57]);
+
+        // DIBUJO BRAZO DE CATAPULTA
+        var m1 = mat4.clone(modelMatrix);
+        mat4.rotate(m1, m1, -Math.PI/2, [0, 1, 0]);
+        mat4.translate(m1, m1, [-7, 4.6, -(this.CATAPULT_WHEEL_SEPARATION_2 / 2)]);
+            // Rotación del brazo en su eje de catapulta
+        mat4.rotate(m1, m1,  50 * Math.PI/180, [0, 0, 1]);
+        this.catapult_arm.draw(m1, rope_lookat);
+
+        // DIBUJO MANIVELA DE EJE DE SOGA 1
+        var m1 = mat4.clone(modelMatrix);
+        mat4.translate(m1, m1, [this.CATAPULT_WHEEL_SEPARATION_1 * 0.2, 1.21, 0.57]);
+        mat4.rotate(m1, m1, Math.PI/2, [0, 0, 1]);
+        this.catapult_crank_1.draw(m1);
+
+        // DIBUJO MANIVELA DE EJE DE SOGA 2
+        var m1 = mat4.clone(modelMatrix);
+        mat4.translate(m1, m1, [this.CATAPULT_WHEEL_SEPARATION_1 * 0.5, 1.21, 0.57]);
+        mat4.rotate(m1, m1, Math.PI/2, [0, 0, 1]);
+        this.catapult_crank_1.draw(m1);
+
+
+    }
+
+    _drawStaticsParts(modelMatrix) {
         // DIBUJO RUEDAS
         this.catapult_wheel_br.draw(modelMatrix)
 
@@ -83,31 +137,7 @@ class Catapult {
         // DIBUJO EJE DE SOGA
 
         var m1 = mat4.clone(modelMatrix);
-        mat4.translate(m1, m1, [this.CATAPULT_WHEEL_SEPARATION_1 * 0.21, 1.8, 0.57]);
+        mat4.translate(m1, m1, [this.CATAPULT_WHEEL_SEPARATION_1 * 0.181, 1.8, 0.57]);
         this.catapult_hub_rope.draw(m1);
-
-        // DIBUJO BRAZO DE CATAPULTA
-
-        var m1 = mat4.clone(modelMatrix);
-        mat4.rotate(m1, m1, -Math.PI/2, [0, 1, 0]);
-        mat4.translate(m1, m1, [-7, 4.6, -(this.CATAPULT_WHEEL_SEPARATION_2 / 2)]);
-            // Rotación del brazo en su eje de catapulta
-        mat4.rotate(m1, m1, Math.PI/7, [0, 0, 1]);
-        this.catapult_arm.draw(m1);
-    }
-
-    setViewProjectionMatrix(projMatrix, viewMatrix) {
-        this.catapult_wheel_br.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_wheel_fr.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_wheel_bl.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_wheel_fl.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_floor.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_column_1.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_column_2.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_column_3.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_column_4.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_hub_principal.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_hub_rope.setViewProjectionMatrix(projMatrix, viewMatrix);
-        this.catapult_arm.setViewProjectionMatrix(projMatrix, viewMatrix);
     }
 }
--- js/world_objects/CatapultArm.js
+++ js/world_objects/CatapultArm.js
@@ -2,9 +2,11 @@ class CatapultArm {
     constructor() {
         this.arm = new Cube(0.3, 0.3, 13, true, MaterialsList.DEFAULT);
         this.bucket = new Cube(1.2, 1.2, 0.6, true, MaterialsList.DEFAULT);
+        this.rope_end = new Cilinder(0.6, 0.5, true, MaterialsList.DEFAULT);
+        this.rope = new Cilinder(0.05, 1, true, MaterialsList.DEFAULT);
     }
 
-    draw(modelMatrix) {
+    draw(modelMatrix, rope_lookat) {
         var m1 = mat4.clone(modelMatrix);
         mat4.translate(m1, m1, [-2, 0, 0]);
         this.arm.draw(m1);
@@ -13,10 +15,34 @@ class CatapultArm {
         mat4.rotate(m1, m1, Math.PI/2, [0, 0, 1]);
         mat4.translate(m1, m1, [-0.3, -(11 + 1.2), 0]);
         this.bucket.draw(m1);
+
+        var m1 = mat4.clone(modelMatrix);
+        mat4.translate(m1, m1, [7.3, 0, 0]);
+        this.rope_end.draw(m1);
+
+        var m1 = mat4.clone(modelMatrix);
+        mat4.translate(m1, m1, [7.55, 0, 0]);
+
+        var eye = vec3.fromValues(m1[12], m1[13], m1[14]);
+        var center = vec3.fromValues(rope_lookat[12], rope_lookat[13], rope_lookat[14]);
+        var distance = vec3.distance(eye, center);
+
+        console.log(distance);
+
+        mat4.targetTo(m1, eye, center, [1, 0, 0]);
+        mat4.rotate(m1, m1, Math.PI/2, [0, 1, 0]);
+
+        console.log(m1);
+        mat4.scale(m1, m1, [distance, 1, 1]);
+        console.log(m1);
+
+        this.rope.draw(m1);      
     }
 
     setViewProjectionMatrix(projMatrix, viewMatrix) {
         this.arm.setViewProjectionMatrix(projMatrix, viewMatrix);
         this.bucket.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.rope_end.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.rope.setViewProjectionMatrix(projMatrix, viewMatrix);
     }
 }
