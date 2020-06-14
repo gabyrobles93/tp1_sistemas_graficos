4359628a662675aed9471eb98e0174db6c4d892a
gabriel.robles <grobles@fi.uba.ar>
2020-06-14 20:30:20 -0300

    Modelo ventana de castillo.

---
+++ js/object2D/RoundedSquare.js
@@ -0,0 +1,56 @@
+class RoundedSquare {
+    constructor() {
+        this.vertices = 12;
+        this.sides = [[0, 0, 0], 
+                [0, 48/18, 0], 
+                [2/18, 52/18, 0],
+                [4/18, 54/18, 0],
+                [8/18, 58/18, 0],
+                [14/18, 60/18, 0],
+                [18/18, 60/18, 0],
+                [24/18, 58/18, 0],
+                [28/18, 54/18, 0],
+                [30/18, 52/18, 0],
+                [32/18, 48/18, 0],
+                [32/18, 0, 0],
+                [0, 0, 0]];
+
+        this.normals = [[0, 0, 1], 
+                        [0, 0, 1], 
+                        [0, 1, 0],
+                        [0, 1, 0],
+                        [1, 0, 0],
+                        [1, 0, 0],
+                        [0, 0, 1],
+                        [0, 0, 1],
+                        [0, 1, 0],
+                        [0, 1, 0],
+                        [1, 0, 0],
+                        [1, 0, 0],
+                        [0, 0, 1]];
+    }
+
+    getPosition(u) {
+        return this.sides[u*12];
+    }
+
+    getNormal(u) {
+        return this.normals[u*12];
+    }
+
+    getTangent(u) {
+        return [0, 0, 0];
+    }
+
+    getBinormal(u) {
+        return [0, 0, 0];
+    }
+
+    getCenterPosition(u) {
+        return [16/18, 28/18, 0];
+    }
+
+    getCenterNormal(u) {
+        return [1, 0, 0];
+    }
+}
--- js/world_objects/castle/CastleFloor.js
+++ js/world_objects/castle/CastleFloor.js
@@ -6,6 +6,10 @@ class CastleFloor {
 
         this.floor = new Cube(size_1, size_2, height, false, MaterialsList.BEIGE);
         this.roof = new Cube(size_1 + 0.3, 0.3, size_2 * 2 + 0.6, true, MaterialsList.BEIGE);
+
+        this.windows = [];
+        this.windows_qty = 0;
+        this._createWindows();
     }
 
     draw(modelMatrix) {
@@ -16,11 +20,30 @@ class CastleFloor {
         mat4.rotate(m1, m1, Math.PI/2, [0, 1, 0]);
         mat4.translate(m1, m1, [-this.size_2 - 0.3, 0, this.height + 0.3]);
         this.roof.draw(m1);
+
+
+        for (var i = 0; i < this.windows_qty; i++) {
+            var m1 = mat4.clone(modelMatrix);
+            mat4.rotate(m1, m1, Math.PI/2, [0, 1, 0]);
+            mat4.translate(m1, m1, [-(this.size_2 + 0.2), 0, 2 * this.height/5]);
+            this.windows[i].draw(m1);
+        }
     }
 
     setViewProjectionMatrix(projMatrix, viewMatrix) {
         this.floor.setViewProjectionMatrix(projMatrix, viewMatrix);
         this.roof.setViewProjectionMatrix(projMatrix, viewMatrix);
+        for (var i = 0; i < this.windows_qty; i++) {
+            this.windows[i].setViewProjectionMatrix(projMatrix, viewMatrix);
+        }
+    }
+
+    // PRIVATE
+
+    _createWindows() {
+        var window = new CastleWindow();
+        this.windows.push(window);
+        this.windows_qty = this.windows_qty + 1;
     }
 
 }
