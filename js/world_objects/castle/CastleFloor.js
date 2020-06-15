9600133b176be36d938dc823cae606c1ecb177c5
gabriel.robles <grobles@fi.uba.ar>
2020-06-14 22:51:43 -0300

    WIP dibujar ventanas

--- js/world_objects/castle/CastleFloor.js
+++ js/world_objects/castle/CastleFloor.js
@@ -4,12 +4,30 @@ class CastleFloor {
         this.size_2 = size_2;
         this.height = height;
 
-        this.floor = new Cube(size_1, size_2, height, false, MaterialsList.BEIGE);
-        this.roof = new Cube(size_1 + 0.3, 0.3, size_2 * 2 + 0.6, true, MaterialsList.BEIGE);
+        this.floor = new Cube(this.size_1, this.size_2, height, false, MaterialsList.BEIGE);
+        this.roof = new Cube(this.size_1 + 0.3, 0.3, this.size_2 * 2 + 0.6, true, MaterialsList.BEIGE);
+        this.windows_size_1 = [];
+        this.window_test = new CastleWindow();
 
-        this.windows = [];
-        this.windows_qty = 0;
-        this._createWindows();
+        this.BORDER_MARGIN = 3;
+        this.MIN_WINDOW_BETWEEN_SIZE = 2;
+        this.WINDOW_SIZE = 1.8;
+        
+        var step = this.size_1 * 2;
+        var prev_step = 0;
+        this.window_points_size_1 = [];
+        while (step/2 >= this.BORDER_MARGIN) {
+            step = step/2;
+            this.window_points_size_1.push(step);
+            if (step + prev_step < this.size_1) {
+                this.window_points_size_1.push(step + prev_step);
+            }
+            prev_step = step;
+        }
+
+        console.log(this.window_points_size_1);
+
+        this._createWindows(this.window_points_size_1);
     }
 
     draw(modelMatrix) {
@@ -21,29 +39,31 @@ class CastleFloor {
         mat4.translate(m1, m1, [-this.size_2 - 0.3, 0, this.height + 0.3]);
         this.roof.draw(m1);
 
+        console.log(this.windows_size_1.length);
 
-        for (var i = 0; i < this.windows_qty; i++) {
+        var m1 = mat4.clone(modelMatrix);
+        mat4.rotate(m1, m1, Math.PI/2, [0, 1, 0]);
+        mat4.translate(m1, m1, [-(this.size_2 + 0.3), -this.FIRST_WINDOW_POSITION, 0.38 * this.height]);
+        this.window_test.draw(m1);
+
+        for (var i = 0; i < this.windows_size_1.length; i++) {
             var m1 = mat4.clone(modelMatrix);
             mat4.rotate(m1, m1, Math.PI/2, [0, 1, 0]);
-            mat4.translate(m1, m1, [-(this.size_2 + 0.2), 0, 2 * this.height/5]);
-            this.windows[i].draw(m1);
+            mat4.translate(m1, m1, [-(this.size_2 + 0.3), this.size_1 - this.window_points_size_1[i] - this.WINDOW_SIZE/2, 0.38 * this.height]);
+            this.windows_size_1[i].draw(m1);
         }
     }
 
     setViewProjectionMatrix(projMatrix, viewMatrix) {
         this.floor.setViewProjectionMatrix(projMatrix, viewMatrix);
         this.roof.setViewProjectionMatrix(projMatrix, viewMatrix);
-        for (var i = 0; i < this.windows_qty; i++) {
-            this.windows[i].setViewProjectionMatrix(projMatrix, viewMatrix);
-        }
+        this.window_test.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.windows_size_1.forEach(window => window.setViewProjectionMatrix(projMatrix, viewMatrix));
     }
 
     // PRIVATE
 
-    _createWindows() {
-        var window = new CastleWindow();
-        this.windows.push(window);
-        this.windows_qty = this.windows_qty + 1;
+    _createWindows(window_points_size_1) {
+        window_points_size_1.forEach(point => this.windows_size_1.push(new CastleWindow()));
     }
-
 }
