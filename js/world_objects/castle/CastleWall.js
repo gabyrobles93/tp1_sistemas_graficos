84e8a9473e5e9178c955e6078d11ec6556e80b55
gabriel.robles <grobles@fi.uba.ar>
2020-06-24 16:44:10 -0300

    Refactor en muralla de castillo.

--- js/world_objects/castle/CastleWall.js
+++ js/world_objects/castle/CastleWall.js
@@ -1,38 +1,18 @@
 class CastleWall {
     constructor(sides_qty, side_size) {
-        this.sides_qty = sides_qty;
+        this.sides_qty = sides_qty - 1;
         this.side_size = side_size;
-        this.side_offset = 7;
-        this.sides = []
-        this.sum_angles = 180 + 180 * (this.sides_qty - 3);
-        this.each_angle = this.sum_angles / this.sides_qty;
 
-        this._createSides();
+        this.wall = new Extrusion(new CastleWall2D(), new Circle(this.sides_qty, this.side_size), false, MaterialsList.WALL_GREY);
     }
 
     draw(modelMatrix) {
-        for(var i = 0; i < this.sides_qty; i++) {
-            var m1 = mat4.clone(modelMatrix);
-            
-            for (var j = 0; j < i; j++) {
-                mat4.translate(m1, m1, [this.side_size - this.side_offset, -this.side_offset, 0]);
-                mat4.rotate(m1, m1, (180 - this.each_angle) * Math.PI / 180, [0, 0, 1]);
-            }
-
-            this.sides[i].draw(m1);
-        }
+        var m1 = mat4.clone(modelMatrix);
+        mat4.translate(m1, m1, [this.side_size/2, this.side_size/2, 0]);
+        this.wall.draw(m1);
     }
 
     setViewProjectionMatrix(projMatrix, viewMatrix) {
-        for (var i = 0; i <= this.sides_qty; i++) {
-            this.sides[i].setViewProjectionMatrix(projMatrix, viewMatrix);
-        }
-    }
-
-    // Private
-    _createSides() {
-        for (var i = 0; i <= this.sides_qty; i++) {
-            this.sides.push(new Extrusion(new CastleWall2D(), new Line(4, this.side_size), false, MaterialsList.WALL_GREY));
-        }
+        this.wall.setViewProjectionMatrix(projMatrix, viewMatrix);
     }
 }
