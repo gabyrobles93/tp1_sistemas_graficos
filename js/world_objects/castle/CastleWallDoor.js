fceb0d0fb4ccecfd14119714a8c9d5be58f88242
gabriel.robles <grobles@fi.uba.ar>
2020-06-24 20:40:43 -0300

    Lado de muarlla con puerta WIP.

--- js/world_objects/castle/CastleWallDoor.js
+++ js/world_objects/castle/CastleWallDoor.js
@@ -2,8 +2,14 @@ class CastleWallDoor {
     constructor(size) {
         this.size = size;
         this.WALL_PROPORTION = 0.25;
+        this.DOOR_FRAME_WIDTH = 0.8;
         this.wall_1 = new Extrusion(new CastleWall2D(), new Line(2, this.size * this.WALL_PROPORTION), false, MaterialsList.WALL_GREY);
         this.wall_2 = new Extrusion(new CastleWall2D(), new Line(2, this.size * this.WALL_PROPORTION), false, MaterialsList.WALL_GREY);
+
+        this.door_frame_1 = new Cube(this.DOOR_FRAME_WIDTH, 8.5, 6.5, true, MaterialsList.WALL_GREY);
+        this.door_frame_2 = new Cube(this.DOOR_FRAME_WIDTH, 8.5, 6.5, true, MaterialsList.WALL_GREY);
+        this.door_frame_3 = new Cube(this.DOOR_FRAME_WIDTH, 8.5, 6.5, true, MaterialsList.WALL_GREY);
+
         this.OFFSET = 5;
     }
 
@@ -15,10 +21,24 @@ class CastleWallDoor {
         var m1 = mat4.clone(modelMatrix);
         mat4.translate(m1, m1, [this.size * (1-this.WALL_PROPORTION) - this.OFFSET, 0, 0]);
         this.wall_2.draw(m1);        
+
+        var m1 = mat4.clone(modelMatrix);
+        mat4.translate(m1, m1, [this.OFFSET, 0, 0]);
+        mat4.rotate(m1, m1, Math.PI/2, [0, 0, -1]);
+        mat4.translate(m1, m1, [4, this.size * this.WALL_PROPORTION + this.DOOR_FRAME_WIDTH, 7.5]);
+        this.door_frame_1.draw(m1);
+
+        var m1 = mat4.clone(modelMatrix);
+        mat4.rotate(m1, m1, Math.PI/2, [0, 0, -1]);
+        mat4.translate(m1, m1, [4, this.size * (1-this.WALL_PROPORTION) - this.OFFSET - this.DOOR_FRAME_WIDTH, 7.5]);
+        this.door_frame_2.draw(m1); 
     }
 
     setViewProjectionMatrix(projMatrix, viewMatrix) {
         this.wall_1.setViewProjectionMatrix(projMatrix, viewMatrix);
         this.wall_2.setViewProjectionMatrix(projMatrix, viewMatrix);
+
+        this.door_frame_1.setViewProjectionMatrix(projMatrix, viewMatrix);
+        this.door_frame_2.setViewProjectionMatrix(projMatrix, viewMatrix);
     }
 }
