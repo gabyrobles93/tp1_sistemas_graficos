e5d805f3a4bb76011d34ba7ef65e0c4d61657e15
gabriel.robles <grobles@fi.uba.ar>
2020-06-24 20:05:59 -0300

    Lado de muarlla con puerta WIP.

--- js/world_objects/castle/CastleWallDoor.js
+++ js/world_objects/castle/CastleWallDoor.js
@@ -1,16 +1,19 @@
 class CastleWallDoor {
     constructor(size) {
         this.size = size;
-        this.wall_1 = new Extrusion(new CastleWall2D(), new Line(2, this.size * 0.3), false, MaterialsList.WALL_GREY);
-        this.wall_2 = new Extrusion(new CastleWall2D(), new Line(2, this.size * 0.3), false, MaterialsList.WALL_GREY);
+        this.WALL_PROPORTION = 0.25;
+        this.wall_1 = new Extrusion(new CastleWall2D(), new Line(2, this.size * this.WALL_PROPORTION), false, MaterialsList.WALL_GREY);
+        this.wall_2 = new Extrusion(new CastleWall2D(), new Line(2, this.size * this.WALL_PROPORTION), false, MaterialsList.WALL_GREY);
+        this.OFFSET = 5;
     }
 
     draw(modelMatrix) {
         var m1 = mat4.clone(modelMatrix);
+        mat4.translate(m1, m1, [this.OFFSET, 0, 0]);
         this.wall_1.draw(m1);
 
         var m1 = mat4.clone(modelMatrix);
-        mat4.translate(m1, m1, [this.size * 0.7, 0, 0]);
+        mat4.translate(m1, m1, [this.size * (1-this.WALL_PROPORTION) - this.OFFSET, 0, 0]);
         this.wall_2.draw(m1);        
     }
 
