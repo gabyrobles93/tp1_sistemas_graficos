d438fa7d69dc14a402273342c2653051c18462d2
gabriel.robles <grobles@fi.uba.ar>
2020-07-27 20:53:34 -0300

    Corrijo normales del rectangulo

--- js/object2D/Rectangle.js
+++ js/object2D/Rectangle.js
@@ -2,26 +2,37 @@ class Rectangle {
     constructor(size_1, size_2) {
         this.size_1 = size_1;
         this.size_2 = size_2;
-        this.vertices = 4;
-        this.sides = [[size_1, size_2, 0], 
-                [-size_1, size_2, 0], 
-                [-size_1, -size_2, 0],
-                [size_1, -size_2, 0],
-                [size_1, size_2, 0]];
+        this.vertices = 9;
 
-        this.normals = [[1, 0, 1], 
-                [0, 0, -1], 
-                [0, 0, -1],
-                [0, 0, 1],
-                [0, 0, 1]];
+        this.sides = [  [size_1, -size_2, 0],
+                        [size_1, -size_2, 0],
+                        [size_1, size_2, 0],
+                        [size_1, size_2, 0],
+                        [-size_1, size_2, 0],
+                        [-size_1, size_2, 0],
+                        [-size_1, -size_2, 0],
+                        [-size_1, -size_2, 0],
+                        [size_1, -size_2, 0],
+                        [size_1, -size_2, 0]];
+
+        this.normals = [[0, -1, 0], 
+                        [1, 0, 0],
+                        [1, 0, 0],
+                        [0, 1, 0],
+                        [0, 1, 0],
+                        [-1, 0, 0],
+                        [-1, 0, 0],
+                        [0, -1, 0],
+                        [0, -1, 0],
+                        [1, 0, 0]];
     }
 
     getPosition(u, level) {
-        return this.sides[u*4];
+        return this.sides[u*this.vertices];
     }
 
     getNormal(u) {
-        return this.normals[u*4];
+        return this.normals[u*this.vertices];
     }
 
     getTangent(u) {
